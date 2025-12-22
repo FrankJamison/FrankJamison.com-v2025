5dad5247843d90f7cbadf632e9e9a6c4e0cf8683
FrankJamison <frank@frankjamison.com>
2025-12-22 00:06:56 -0800

    removed files

--- portfolio/DnD5eTools (2021)/js/app.js
+++
@@ -1,7 +0,0 @@
-// const tasksService = new TasksService();
-// const todo = new ToDo(tasksService);
-
-const charactersService = new CharactersService();
-const characterList = new CharacterList(charactersService);
-
-characterList.init();
--- portfolio/DnD5eTools (2021)/js/getCharacterSelection.js
+++
@@ -1,5 +0,0 @@
-const doGetCharacters = async (e) => {
-    e.preventDefault();
-
-    characterList.init();
-};
